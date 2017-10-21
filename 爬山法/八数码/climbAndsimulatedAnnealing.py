19c624dd864b0330391aff4d4207aa368c0e1e30
jiaxin96 <572673807@2qq.com>
2017-10-21 15:18:26 +0800

    八数码和八皇后问题的的基本计算算法完成 开始完成统计性能部分

--- 爬山法/八数码/climbAndsimulatedAnnealing.py
+++ 爬山法/八数码/climbAndsimulatedAnnealing.py
@@ -11,17 +11,17 @@ import math
 
 def schdule(time_count):
     init_temperature = 1000
-    coolingRate = 0.9999
+    coolingRate = 0.9997
     return  init_temperature*(coolingRate**time_count)
 
 
 def simulated_annealing(initBoard):
     cur_board = initBoard.copy()
     step = 0
-    directions = [1,2,3,4]
+    directions = [0,1,2,3]
     time_count = 0
-
-    while True:
+    temperature = schdule(0)
+    while temperature > 0.0003:
         temperature = schdule(time_count)
         time_count += 1
 
@@ -38,16 +38,14 @@ def simulated_annealing(initBoard):
                     break
         if (len(neighbor) != 0):
             cur_board = neighbor[0]
-        else:
-            local_smallest = cur_board
-            break
+    local_smallest = cur_board
     return local_smallest, step
 
 
 def simple_climb_first_select(initBoard):
     cur_board = initBoard.copy()
     step = 0
-    directions = [1,2,3,4]
+    directions = [0,1,2,3]
     while True:
         cur_evaluation = pt.evaluation(cur_board)
         neighbor = []
@@ -66,15 +64,55 @@ def simple_climb_first_select(initBoard):
             break
     return local_smallest, step
 
+def simple_climb_optimistic(initBoard):
+    cur_board = initBoard.copy()
+    step = 0
+    directions = [0,1,2,3]
+
+    while True:
+        cur_evaluation = pt.evaluation(cur_board)
+        min_board = cur_board
+        min_board_evaluation = cur_evaluation
+        neighbor = []
+        step += 1
+        for direction in directions:
+            next_board = cur_board.copy()
+            if pt.move(direction, next_board):
+                next_board_evaluation = pt.evaluation(next_board)
+                if (next_board_evaluation<min_board_evaluation):
+                    min_board_evaluation = next_board_evaluation
+                    min_board = next_board
+        if (min_board_evaluation == cur_evaluation):
+            local_smallest = min_board
+            break
+        cur_board = min_board
+    return local_smallest, step
+
 
 def once_test():
     init_board = pt.gen(100)
-    print(pt.evaluation(init_board))
     print(init_board)
+    print(pt.evaluation(init_board))
     (ans_board, ans_step) = simple_climb_first_select(init_board)
     print(ans_board)
     print(pt.evaluation(ans_board))
     print(ans_step)
+    (ans_board, ans_step) = simple_climb_optimistic(init_board)
+    print(ans_board)
+    print(pt.evaluation(ans_board))
+    print(ans_step)
+    (ans_board, ans_step) = simulated_annealing(init_board)
+    print(ans_board)
+    print(pt.evaluation(ans_board))
+    print(ans_step)
+
+def one_case_test():
+    init_board = np.array([3, 8, 1, 4, 5, 6, 7, 2, 0])
+    print(pt.evaluation(init_board))
+    pt.move(3, init_board)
+    print(init_board)
+    print(pt.evaluation(init_board))
+
 
 if __name__ == '__main__':
     once_test()
